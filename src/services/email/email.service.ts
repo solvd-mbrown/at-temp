119b84c86b3c537fe87e78c88044044cb3b1af29
kterekhin-solvd <82385895+kterekhin-solvd@users.noreply.github.com>
2023-02-02 15:57:07 +0200

    Fixed pagination

--- src/services/email/email.service.ts
+++ src/services/email/email.service.ts
@@ -1,5 +1,4 @@
 import { Inject, Injectable } from "@nestjs/common";
-import { clearGlobalAppDefaultCred } from "firebase-admin/lib/app/credential-factory";
 import { FileService } from "src/domain/file/file.service";
 import { UserRepository } from "src/domain/user/user.repository";
 import { EmailProvider, IEmailProvider } from "./email.interface";
@@ -14,10 +13,13 @@ export class EmailService {
 
   public async generateAndSendStoreReport(emails: string[]) {
     const users = await this.userRepository.getUsersWithStorageFileId();
-    const report = await this.fileService.getUploadSizeByEmailReport(users);
-    return await this.emailProvider.sendEmail({
-      emails,
-      attachments: [report],
-    });
+    setTimeout(async () => {
+      const report = await this.fileService.getUploadSizeByEmailReport(users);
+      await this.emailProvider.sendEmail({
+        emails,
+        attachments: [report],
+      });
+    }, 0);
+    return { status: "report request received" };
   }
 }
