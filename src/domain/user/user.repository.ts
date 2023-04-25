184443a7d1f0e04e6326925bf1c97d7080c3e8bc
Vitalii Shumko <vshumko@solvd.com>
2023-04-25 11:15:37 +0300

    minor fix.

--- src/domain/user/user.repository.ts
+++ src/domain/user/user.repository.ts
@@ -187,9 +187,6 @@ export class UserRepository {
           "User.pets": params?.pets
             ? UtilsRepository.getStringVersion(params?.pets)
             : null,
-          "User.setting": params?.setting
-            ? UtilsRepository.getStringVersion(params?.setting)
-            : null,
           "User.bornAddress": params?.bornAddress
             ? UtilsRepository.getStringVersion(params?.bornAddress)
             : null,
